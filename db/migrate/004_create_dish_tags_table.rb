5eff17c1985f5d90c263e8db3804162bfecf6e13
xoliio <xoliio@gmail.com>
2020-10-27 16:25:11 -0700

    not even closed to finished yet. with Kim

---
+++ db/migrate/004_create_dish_tags_table.rb
@@ -0,0 +1,10 @@
+class CreateDishTagsTable < ActiveRecord::Migration[5.2]
+
+    def change
+        create_table :dish_tags do |t|
+            t.integer :tag_id
+            t.integer :dish_id
+        end
+    end
+
+end
